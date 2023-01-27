e2dd0ab330fb2f07138b9eada0dd8d6aeb4a62de
kessius xubuntu <kes.olling00@gmail.com>
2023-01-27 19:26:17 +0100

    Use the progressbar.js and make function in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// function make 
+let now = new Date()
+
+function elapsedDays(currentDay, currentMonth, yearSize){
+    
+
+}
+
+
+
+function yearSize(currentYear){
+    //makes a decision if year is leap year and gives amount of days
+    if((currentYear-2020)%4==0){
+        return 366
+    } else { return 365 }
+}
